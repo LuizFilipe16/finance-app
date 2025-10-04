2cb27c388af8ecd7f827701f4f3a7b7e000e7912
Luiz Filipe <filipe.oli2016@gmail.com>
2025-10-04 15:02:59 -0300

    feat: add PostgresDeleteUserRepository

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,14 @@
+import { PostgresHelper } from '../../db/postgres/helper';
+
+export class PostgressDeleteUser {
+    async execute(userId) {
+        const deleteUser = await PostgresHelper.query(
+            `DELETE FROM users 
+            WHERE id = $1 
+            RETURNING *`,
+            [userId],
+        );
+
+        return deleteUser[0];
+    }
+}
